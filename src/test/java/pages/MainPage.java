3a32e94b07f7035def099bab7f885594564d6e08
100nout.by <pavelrusachok@gmail.com>
2021-02-05 11:11:45 +0300

    New selectors

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -11,45 +11,56 @@ public class MainPage extends BasePage {
     public MainPage(WebDriver driver) {
         super(driver);
     }
+    private By pasteField = By.id("postform-text");
+    private By syntaxField = By.id("select2-postform-format-container");
+    private By syntaxPushing = By.className("select2-search__field");
+    private By pasteExpirationPushing = By.id("select2-postform-expiration-container");
+    private By pasteExpirationSelection = By.xpath("//li[text()='10 Minutes']");
+    private By pasteNameFieldPushing = By.id("postform-name");
+    private By newPasteButton = By.xpath("//button[@class='btn -big']");
+    private By textFromNameMath = By.xpath("//div[@class='info-top']/child::h1");
+    private By textFromSyntaxButton = By.xpath("//div[@class='left']/child::a");
+    private By textFromDataField = By.xpath("//textarea[@class='textarea']");
+
     public void openPage(){
         driver.get("https://pastebin.com");
     }
 
     public void writeTextInToNewPasteField(String textForNewPaste){
-        driver.findElement(By.id("postform-text")).sendKeys(textForNewPaste);
+        driver.findElement(pasteField).sendKeys(textForNewPaste);
     }
 
     public void selectSyntaxHighlighting(String syntax){
-        driver.findElement(By.id("select2-postform-format-container")).click();
-        WebElement syntaxSelection = driver.findElement(By.className("select2-search__field"));
+        driver.findElement(syntaxField).click();
+        WebElement syntaxSelection = driver.findElement(syntaxPushing);
         syntaxSelection.sendKeys(syntax);
         syntaxSelection.sendKeys(Keys.ENTER);
     }
     public void selectPasteExpiration(){
-        driver.findElement(By.id("select2-postform-expiration-container")).click();
-        driver.findElement(By.xpath("//li[text()='10 Minutes']")).click();
+        driver.findElement(pasteExpirationPushing).click();
+        driver.findElement(pasteExpirationSelection).click();
     }
 
     public void writeTextInToPasteNameField(String textForPasteName){
-        driver.findElement(By.id("postform-name")).sendKeys(textForPasteName);
+        driver.findElement(pasteNameFieldPushing).sendKeys(textForPasteName);
     }
 
     public void pressCreateNewPasteButton(){
-        driver.findElement(By.xpath("//button[@class='btn -big']")).click();
+        driver.findElement(newPasteButton).click();
     }
 
     public void checkPasteNameMath(String textForPasteName){
-        String isExist = driver.findElement(By.xpath("//div[@class='info-top']/child::h1")).getText();
+        String isExist = driver.findElement(textFromNameMath).getText();
         assertEquals(isExist,textForPasteName);
     }
 
     public void checkTextOnSyntaxButton(String syntax){
-        String isExist = driver.findElement(By.xpath("//div[@class='left']/child::a")).getText();
+        String isExist = driver.findElement(textFromSyntaxButton).getText();
        assertEquals(isExist,syntax);
     }
 
     public void checkTextInRawPasteDataField(String textInPasteData){
-        String isExist = driver.findElement(By.xpath("//textarea[@class='textarea']")).getText();
+        String isExist = driver.findElement(textFromDataField).getText();
         assertEquals(isExist,textInPasteData);
     }
 }
